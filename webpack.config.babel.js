53c7e55f363641941bfc18438d31b068fee4a5fa
Kagami Hiiragi <kagami@genshiken.org>
2017-10-27 00:28:00 +0300

    Fix assets loaders

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -60,9 +60,8 @@ export default {
     noParse: inmodules(".*json-schema", "lib", "validate\\.js"),
     loaders: [
       {test: /\.json$/, loader: "json-loader"},
+      {test: /\.(png|exe)$/, loader: "file-loader", query: FULLNAMEQ},
       {test: insrc(".+\\.js"), loader: "babel-loader"},
-      {test: insrc(".+\\.png"), loader: "file-loader", query: FULLNAMEQ},
-      {test: insrc(".+\\.exe"), loader: "file-loader", query: FULLNAMEQ},
       {test: insrc("index", "index\\.html\\.ejs"), loaders: INDEX_LOADERS},
       {test: insrc("index", "package\\.json\\.ejs"), loaders: PACKAGE_LOADERS},
       // Fonts, font-awesome.
